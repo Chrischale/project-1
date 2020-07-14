c1a2e69a88b60276306b61290e7a16388576832f
Chrischale Panditharathne <cpanda27@bu.edu>
2020-07-13 21:13:22 -0400

    hi

--- src/Remote/ers-0/index.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const ersClient = axios.create({
-    baseURL: 'http://localhost:2020',
-    headers:{
-        'Content-Type' : 'application/json'
-    },
-    withCredentials : true
-})
